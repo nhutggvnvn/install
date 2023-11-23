49954a01b3fe3df1206e186c90fac8398d53c84f
installapp <nhutgg.paypal.97@gmail.com>
2023-11-23 16:40:51 +0700

    ccccc

--- js/plausible.js
+++
@@ -1,6 +0,0 @@
- var plausibleScript = document.createElement("script");
-        plausibleScript.src = "https://plausible.io/js/plausible.js"
-        plausibleScript.setAttribute("asnyc", "");
-        plausibleScript.setAttribute("defer", "");
-        plausibleScript.setAttribute("data-domain", "bio.link");
-        document.head.appendChild(plausibleScript);
